579f35b7a7f3def3ed552ce2fab992ba4685ab6e
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-05 14:56:29 +0530

    Add project files.

---
+++ ConsoleApp1Encryptor1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Encrypt;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hiii ");
+Encrypt.Programs.Mmmain();
+Programs.Mmmain();  
+
+namespace Encrypt
+{
+
+    public class Programs
+    {
+
+        public static void Mmmain()
+        {
+            Console.WriteLine("Hello *");
+           
+
+           
+        }
+
+
+    }
+
+
+}
+
+
